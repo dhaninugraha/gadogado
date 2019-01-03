fc96478fd24f95bd8ef8eb20a8fd0d9e49dc4c8c
Nugraha Nandiwardhana <dhaninugraha@gmail.com>
2019-01-03 15:17:42 +0700

    added ability to exclude HTML tags

---
+++ dummy.go
@@ -0,0 +1,33 @@
+package gadogado
+
+type dummyStruct struct{}
+
+type dummyMap map[string]dummyStruct
+
+func newDummyMap() *dummyMap {
+	m := make(dummyMap)
+	return &m
+}
+
+func (m *dummyMap) addKey(k string) {
+	if k != "" {
+		(*m)[k] = dummyStruct{}
+	}
+}
+
+func (m *dummyMap) Exists(k string) bool {
+	if m == nil || k == "" {
+		return false
+	}
+
+	if m != nil {
+		_, ok := (*m)[k]
+		if ok {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	return false
+}
--- gadogado.go
+++ gadogado.go
@@ -17,7 +17,7 @@ func newNode() Node {
 	return Node{Children: []Node{}, Attrs: map[string]string{}}
 }
 
-func iterateNodes(n *html.Node, parent *Node) {
+func iterateNodes(n *html.Node, parent *Node, excludedTags *dummyMap) {
 	if n.Type == html.ElementNode {
 		parent.TagName = n.Data
 
@@ -37,21 +37,36 @@ func iterateNodes(n *html.Node, parent *Node) {
 
 	for child := n.FirstChild; child != nil; child = child.NextSibling {
 		if child.Type == html.ElementNode {
-			// if tag name (child.Data) not in ignored tags {
+			if !excludedTags.Exists(child.Data) {
 				childNode := newNode()
 				parent.Children = append(parent.Children, childNode)
-				iterateNodes(child, &parent.Children[len(parent.Children) - 1])
-			// }
+				iterateNodes(child, &parent.Children[len(parent.Children) - 1], excludedTags)
+			}
 		}
 	}
 }
 
-func Make(r io.Reader) (*Node, error) {
+func ExcludeTags(tags ...string) *dummyMap {
+	excluded := newDummyMap()
+
+	for _, tag := range tags {
+		excluded.addKey(tag)
+	}
+
+	return excluded
+}
+
+func Make(r io.Reader, excludedTags *dummyMap) (*Node, error) {
 	doc, err := html.Parse(r)
 	node := newNode()
 	if err == nil {
-		iterateNodes(doc, &node)
-		return &node.Children[0], nil
+		iterateNodes(doc, &node, excludedTags)
+
+		if len(node.Children) > 0 {
+			return &node.Children[0], nil
+		} else {
+			return &node, err	
+		}
 	} else {
 		return &node, err
 	}
